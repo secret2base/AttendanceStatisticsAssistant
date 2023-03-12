51a68a9ad038b4a398433061b209c102156d33c0
Moritaka <842788849@qq.com>
2023-03-12 10:36:38 +0800

    v1.0

---
+++ Headers.py
@@ -0,0 +1,44 @@
+headerLogin={
+    "X-Service-Id": "userauth",
+    "client_id": "eplus_app",
+    "User-Agent": "SmartOffice/2.6.0 (MuMu;Android6.0.1);",
+    "Connection": "close,",
+    "user_id":"",
+    "Authorization":"",
+    "Content-Type": "application/json;charset=UTF-8",
+    "Content-Length": "70",
+    "Host": "v2-app.delicloud.com",
+    "Accept-Encoding": "gzip"
+
+}
+
+headerOrgId={
+    "X-Service-Id": "organization",
+    "client_id": "eplus_app",
+    "User-Agent": "SmartOffice/2.6.0 (MuMu;Android6.0.1);",
+    "Content-Type": "application/json;charset=UTF-8",
+    "Connection": "close",
+    "Host":"v2-app.delicloud.com",
+    "Accept-Encoding":"gzip"
+}
+
+headerOriginId={
+    "client_id": "eplus_app",
+    "Content-Type": "application/json;charset=UTF-8",
+    "X-Service-Id": "organization",
+    "User-Agent": "SmartOffice/2.6.0 (MuMu;Android6.0.1);",
+    "Connection": "close",
+    "Host": "v2-app.delicloud.com",
+    "Accept-Encoding": "gzip"
+}
+
+
+
+headerGoal = {
+    "client_type": "eplus_app",
+    "Content-Type": "application/json",
+    "Host": "kq.delicloud.com",
+    "Connection": "Keep-Alive",
+    "Accept-Encoding": "gzip",
+    "User-Agent": "okhttp/3.11.0"
+}
--- README.md
+++ README.md
@@ -1,27 +1,16 @@
-# AttendanceStaticsAssistant
-得力e+APP打卡时间统计助手，用于统计截止使用时刻，当周还需多少打卡时间
-记录写代码过程中遇到的一些问题
+### AttendanceStatisticsAssistanc  v1.0
+得力e+ APP打卡时间自动统计工具
 
-从有想法到最终打包exe完成花费约两天时间，成品效果勉强能用
+相较于beta版,1.0版本更新如下
 
-## 问题一：APP内部网址的抓取
-由于得力e+APP隐藏了内部链接，所以通过模拟器+Fiddler抓包的方式得到考勤时间记录，做之前没想到这一步这么简单
+ - 全自动登录和读取信息,不会再受sid改变的影响
+ - 修复了少量文本错误
+ 
+#### 使用须知
+请将登录使用的手机号和密码分别卸载userInformation.txt文件的第一行和第二行
 
-## 问题二：动态网址数据的抓取
-上一步中得到的网站是动态网站，因此之前用过的requests库抓不到要用的数据，换用selenium库抓取  
-并用selenium的模拟点击功能顺带爬了上个月的数据，便于跨月周的计算（xpath定位元素可以直接从检查中导出）
+#### To do list
 
-## 问题三：网站具体数据解析
-上一步得到了网站的全部源码，用beautifulsoup4解析匹配具体位置数据并记录
-
-## 问题四：UI模块
-没学过pyqt5，懒得学，做得很简陋
-
-## 问题五：pyinstaller打包
-一开始是打包缺模块，用venv解决，以免打包目录和pycharm目录不一致  
-然后是运行会闪现命令行，修改了selenium的源代码，但是仍未完全解决  
-打包的exe过大问题，新开一个venv只加入必要的模块，再加上UPX，将原来40+MB的exe优化到了30+MB（虽然还是很大）
-
-## 效果图
-![3SKDV3AS}J{X~ES_KP @I4X](https://user-images.githubusercontent.com/56437903/218032927-8516c6ca-603b-49d5-97c8-8511724a4fac.png)
-# example
+ - 偶现list out of range错误(关闭重启即可)
+ - selenium部分使用requests重写
+ - 代码重构，文档注释的规范化
